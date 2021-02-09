a1821c0cbaed11665f7f7e9d41706689758f68b3
KarlokCheung <georgecheung07@gmail.com>
2021-02-09 15:47:53 +1100

    Add packages, add studentController

---
+++ src/main/java/com/jr/restapi/controllers/StudentController.java
@@ -0,0 +1,16 @@
+package com.jr.restapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @GetMapping
+    public ResponseEntity<String> createStudent(){
+        return ResponseEntity.ok("Hello world");
+    }
+}
