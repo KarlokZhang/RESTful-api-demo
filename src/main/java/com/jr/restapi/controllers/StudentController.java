6351018cd9013a708861cd699decd0066221ac94
KarlokCheung <georgecheung07@gmail.com>
2021-02-10 21:58:12 +1100

    Add Student Entity, GetDto and properties config

--- src/main/java/com/jr/restapi/controllers/StudentController.java
+++ src/main/java/com/jr/restapi/controllers/StudentController.java
@@ -1,6 +1,9 @@
 package com.jr.restapi.controllers;
 
+import com.jr.restapi.dtos.StudentGetDto;
 import com.jr.restapi.dtos.StudentPostDto;
+import com.jr.restapi.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/students")
 public class StudentController {
 
+    @Autowired
+    StudentService studentService;
+
     @PostMapping
-    public ResponseEntity<String> createStudent(@RequestBody StudentPostDto studentPostDto){
-        System.out.println(studentPostDto);
-        return ResponseEntity.ok("Hello world");
+    public ResponseEntity<StudentGetDto> createStudent(@RequestBody StudentPostDto studentPostDto){
+        StudentGetDto studentGetDto = studentService.createStudent(studentPostDto);
+
+        return ResponseEntity.ok(studentGetDto);
     }
 }
